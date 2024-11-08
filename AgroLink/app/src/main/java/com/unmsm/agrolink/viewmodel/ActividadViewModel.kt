8a16ea894f9542fc984dc1d849b90d38e26bace1
Daniel-Loli <nilton.loli@unmsm.edu.pe>
2024-11-08 09:28:34 -0500

    agregando viewmodel para actividades

---
+++ AgroLink/app/src/main/java/com/unmsm/agrolink/viewmodel/ActividadViewModel.kt
@@ -0,0 +1,29 @@
+// ActividadViewModel.kt
+
+package com.unmsm.agrolink.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.unmsm.agrolink.data.DatabaseHelper
+import com.unmsm.agrolink.models.Actividad
+
+class ActividadViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val dbHelper = DatabaseHelper(application)
+
+    private val _actividades = MutableLiveData<List<Actividad>>()
+    val actividades: LiveData<List<Actividad>> get() = _actividades
+
+    // Carga las actividades para un cultivo específico
+    fun loadActividades(cultivoId: Int) {
+        _actividades.value = dbHelper.getActividadesPorCultivo(cultivoId)
+    }
+
+    // Agregar una nueva actividad a un cultivo
+    fun agregarActividad(cultivoId: Int, tipoActividad: String, fecha: String, notas: String, cantidad: Double? = null) {
+        dbHelper.insertActividad(cultivoId, tipoActividad, fecha, notas, cantidad)
+        loadActividades(cultivoId)  // Recarga las actividades después de agregar una nueva
+    }
+}
