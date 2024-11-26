cf7bdd709b14dc312c65f1fcdaca37f951e7a72b
unmsm-jcrs <jair.cachay@unmsm.edu.pe>
2024-11-26 13:20:26 -0500

    Arreglo de frontend  Ver Clima

--- AgroLink/app/src/main/java/com/unmsm/agrolink/ui/ClimaScreen.kt
+++ AgroLink/app/src/main/java/com/unmsm/agrolink/ui/ClimaScreen.kt
@@ -1,5 +1,6 @@
 package com.unmsm.agrolink.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -18,42 +19,65 @@ fun ClimaScreen(fetchWeatherData: suspend () -> Map<String, Any>?) {
     var weatherCondition by remember { mutableStateOf("Cargando...") }
     var errorMessage by remember { mutableStateOf("") }
 
-    LaunchedEffect(Unit) {
-        stage = "loading"
-        val data = fetchWeatherData()
-        if (data != null) {
-            stage = "loaded"
-            temperature = "${data["temp"]} °C"
-            humidity = "${data["humidity"]} %"
-            weatherCondition = data["description"] as String
-        } else {
-            stage = "error"
-            errorMessage = "No se pudo cargar el clima. Verifica tu conexión o clave API."
-        }
-    }
 
-    Box(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        when (stage) {
-            "initial" -> Text("¡Mira el clima!", style = MaterialTheme.typography.titleLarge)
-            "loading" -> CircularProgressIndicator()
-            "loaded" -> Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text("Clima Actual", style = MaterialTheme.typography.titleLarge)
-                Text("Temperatura: $temperature", fontSize = 20.sp)
-                Text("Humedad: $humidity", fontSize = 20.sp)
-                Text("Condición: $weatherCondition", fontSize = 20.sp)
-            }
-            "error" -> Text(
-                text = errorMessage,
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodyLarge,
-                textAlign = TextAlign.Center
+    Scaffold{ padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize() // Llenar el ancho disponible
+                .padding(20.dp),
+        ) {
+            Text(
+                text = "Ver Clima",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSecondary,
+                modifier = Modifier
             )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LaunchedEffect(Unit) {
+                stage = "loading"
+                val data = fetchWeatherData()
+                if (data != null) {
+                    stage = "loaded"
+                    temperature = "${data["temp"]} °C"
+                    humidity = "${data["humidity"]} %"
+                    weatherCondition = data["description"] as String
+                } else {
+                    stage = "error"
+                    errorMessage = "No se pudo cargar el clima. Verifica tu conexión o clave API."
+                }
+            }
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.outline,
+                        shape = MaterialTheme.shapes.small
+                    )
+                    .padding(16.dp)
+            ) {
+                when (stage) {
+                    "initial" -> Text("¡Mira el clima!", style = MaterialTheme.typography.displayLarge)
+                    "loading" -> CircularProgressIndicator()
+                    "loaded" -> Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text("Clima Actual", style = MaterialTheme.typography.displayLarge)
+                        Text( "$temperature", fontSize = 40.sp)
+                        Text("Humedad: $humidity", fontSize = 20.sp)
+                        Text("Condición: $weatherCondition", fontSize = 20.sp)
+                    }
+                    "error" -> Text(
+                        text = errorMessage,
+                        color = MaterialTheme.colorScheme.error,
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
--- AgroLink/app/src/main/java/com/unmsm/agrolink/ui/theme/Shape.kt
+++ AgroLink/app/src/main/java/com/unmsm/agrolink/ui/theme/Shape.kt
@@ -4,6 +4,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
-    medium = RoundedCornerShape(bottomStart = 18.dp, topEnd = 18.dp)
+    small = RoundedCornerShape(25.dp),
+    medium = RoundedCornerShape(bottomStart = 18.dp, topEnd = 18.dp),
 )
